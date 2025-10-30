983b1464bb3b0a3df95893b165e61c679574f32c
25sh0363-code <25sh0363@hyd.silveroaks.co.in>
2025-10-31 00:43:33 +0530

    Remove comments from configuration settings

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-# Configuration settings for the news summarizer
 
-# Model configurations
 MODELS = {
     'extractive': {
         'name': 'bert-base-uncased',
@@ -18,21 +16,21 @@ MODELS = {
     }
 }
 
-# Text preprocessing settings
+
 TEXT_CLEANING = {
     'remove_special_chars': True,
     'remove_stopwords': True,
     'language': 'english'
 }
 
-# UI configurations
+
 UI_CONFIG = {
-    'max_input_length': 10000,  # Maximum characters allowed in text input
+    'max_input_length': 10000,  
     'default_summary_sentences': 3,
     'max_summary_sentences': 10
 }
 
-# Cache settings
+
 CACHE_CONFIG = {
     'model_cache_dir': '.model_cache',
     'enable_caching': True
